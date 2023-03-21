b55637e08311dc8bdadaf8a8a7b3f1f2ebbd26e2
zhiyisoh <eririsepanda@gmail.com>
2023-03-21 16:38:50 +0800

    add and animate background to scrolling

---
+++ app/src/main/java/com/example/rjzr/GameView.java
@@ -0,0 +1,82 @@
+package com.example.rjzr;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.SurfaceView;
+import android.content.Context;
+
+public class GameView extends SurfaceView implements Runnable {
+    private Thread thread;
+    private boolean isRunning;
+    private Paint paint;
+    private Background background1, background2;
+    private int screenX, screenY = 0;
+    public static float screenRatioX, screenRatioY;
+
+    public GameView(MainGameView activity, int screenX, int screenY){
+        super(activity);
+        this.screenX = screenX;
+        this.screenY = screenY;
+        screenRatioX = 1920f / screenX;
+        screenRatioY = 1080f / screenY;
+
+        background1 = new Background(screenX, screenY, getResources());
+        background2 = new Background(screenX, screenY, getResources());
+    }
+
+    @Override
+    public void run(){
+        while(isRunning){
+            toUpdate();
+            toDraw();
+            toSleep();
+        }
+    }
+
+    private void toSleep() {
+        try{
+            Thread.sleep(10);
+        } catch(InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void toDraw() {
+        if (getHolder().getSurface().isValid()) {
+            Canvas canvas = getHolder().lockCanvas();
+            canvas.drawBitmap(background1.background, background1.x, background1.y, paint);
+            canvas.drawBitmap(background2.background, background2.x, background2.y, paint);
+            getHolder().unlockCanvasAndPost(canvas);
+        }
+    }
+
+    private void toUpdate() {
+        background1.y -= (40 * screenRatioY);
+        if (background1.y + this.screenY < 0){
+            background1.y = screenY;
+        }
+        if (background1.y < 0) {
+            background2.y = background1.y + this.screenY;
+        }
+        else {
+            background2.y = background1.y - this.screenY;
+        }
+    }
+
+    public void resume(){
+        isRunning = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public void pause(){
+        try{
+            isRunning = false;
+            thread.join();
+        } catch(InterruptedException e){
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/rjzr/MainGameView.java
@@ -0,0 +1,39 @@
+package com.example.rjzr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainGameView extends AppCompatActivity {
+    private GameView gameView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        Point point = new Point();
+        getWindowManager().getDefaultDisplay().getSize(point);
+        gameView = new GameView(this, point.x, point.y);
+
+        setContentView(gameView);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        gameView.pause();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        gameView.resume();
+    }
+
+}
