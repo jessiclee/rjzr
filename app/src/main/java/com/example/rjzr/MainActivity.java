197ad7d1df5cff49ffe73e14fa77e173b6b3b0a8
Jess <jesstohxl@gmail.com>
2023-04-04 18:34:52 +0800

    highscore in main page

--- app/src/main/java/com/example/rjzr/MainActivity.java
+++ app/src/main/java/com/example/rjzr/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.rjzr;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.media.MediaPlayer;
 import android.os.Bundle;
@@ -10,19 +11,23 @@ import android.view.View;
 import android.content.Context;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mediaPlayer;
-
     DatabaseHelper mdb;
+
+    String getHighScoreQuery = "SELECT * FROM score_entries WHERE _id=1";
+    Cursor cursor = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         View decorView = getWindow().getDecorView();
         // Hide the status bar.
@@ -33,6 +38,18 @@ public class MainActivity extends AppCompatActivity {
 
         mdb = new DatabaseHelper(this);
         SQLiteDatabase sqLiteDatabase = mdb.getWritableDatabase();
+
+        cursor = sqLiteDatabase.rawQuery(getHighScoreQuery, null);
+
+        TextView mainHighscore = (TextView) findViewById(R.id.main_highscore);
+
+        String actualHS = "test";
+        if (cursor.moveToFirst()) {
+            do {
+                actualHS = cursor.getString(1);
+            } while (cursor.moveToNext());
+        }
+        mainHighscore.setText("Highscore: " + actualHS ); //set text for text view
 
         findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
             @Override
