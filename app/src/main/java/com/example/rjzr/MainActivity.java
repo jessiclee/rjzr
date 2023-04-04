01a3ff2b094c694346cc12a651605f0e17cd2096
abiyaimmaculate10 <127825862+abiya-ai@users.noreply.github.com>
2023-04-04 16:26:12 +0800

    audio test

--- app/src/main/java/com/example/rjzr/MainActivity.java
+++ app/src/main/java/com/example/rjzr/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.rjzr;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Context;
@@ -13,10 +14,13 @@ import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
+    MediaPlayer mediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         View decorView = getWindow().getDecorView();
         // Hide the status bar.
         int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
@@ -30,6 +34,14 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.musicc);
+        mediaPlayer.start();
+
+        mediaPlayer.setLooping(true);
+
+
+
 
     }
 }
